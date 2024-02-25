98064ecc7252f75e38ffeeb8182a72dad7648ef6
Abel1 <cainasbel34@gmail.com>
2024-02-25 18:11:42 -0500

    Creación Controlador Categoría

---
+++ Shooping/Shooping/Controllers/CategoriesController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shooping.Data;
+using Shooping.Data.Entities;
+
+namespace Shooping.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET:
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if(ModelState.IsValid) 
+            {
+                try
+                {
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException) 
+                {
+                    if(dbUpdateException.InnerException.Message.Contains("Duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una categoría con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(category);
+        }
+
+        // GET:
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories.FindAsync(id);
+            if(category == null) 
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid) 
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException) 
+                {
+                    if(dbUpdateException.InnerException.Message.Contains("duplicate")) 
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una categoría con el mismo nombre.");
+                    }
+                    else 
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);   
+                    }
+                }
+                catch(Exception exception) 
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(category);
+        }
+
+    }
+}
