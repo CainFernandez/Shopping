deb4400e51df088c66ed1b0fa4aaa24595faf731
CainPerez <cainasbel34@gmail.com>
2024-02-27 21:57:27 -0500

    Creación Controlador categorías
    - Crear.
    - Editar.
    - Detalles.
    -Borrar.

--- Shooping/Shooping/Controllers/CategoriesController.cs
+++ Shooping/Shooping/Controllers/CategoriesController.cs
@@ -29,7 +29,7 @@ namespace Shooping.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Category category)
         {
-            if(ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 try
                 {
@@ -37,9 +37,9 @@ namespace Shooping.Controllers
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateException dbUpdateException) 
+                catch (DbUpdateException dbUpdateException)
                 {
-                    if(dbUpdateException.InnerException.Message.Contains("Duplicate"))
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
                         ModelState.AddModelError(string.Empty, "Ya existe una categoría con el mismo nombre.");
                     }
@@ -50,11 +50,11 @@ namespace Shooping.Controllers
                 }
                 catch (Exception exception)
                 {
-                ModelState.AddModelError(string.Empty, exception.Message);
+                    ModelState.AddModelError(string.Empty, exception.Message);
                 }
             }
             return View(category);
-        }
+    }
 
         // GET:
         public async Task<IActionResult> Edit(int? id)
@@ -107,6 +107,51 @@ namespace Shooping.Controllers
                 }
             }
             return View(category);
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null )
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //GET: Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Category category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
     }
