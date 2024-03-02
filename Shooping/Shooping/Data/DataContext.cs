840d91ae75259cd9f337f57e00bef1fbbf6f9dfd
CainPerez <cainasbel34@gmail.com>
2024-03-01 20:38:36 -0500

    Inclusión a BD las entidades Estado y Ciudad

--- Shooping/Shooping/Data/DataContext.cs
+++ Shooping/Shooping/Data/DataContext.cs
@@ -11,15 +11,23 @@ namespace Shooping.Data
 
         public DbSet<Category> Categories { get; set; }
 
+        public DbSet<City> Cities { get; set; }
+
         public DbSet<Country> Countries { get; set; }
+
+        public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            //Indice Unico.
+            // Indice Unico.
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+            // Indice Compuesto.
+            modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
+            modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
 
         }
     }
