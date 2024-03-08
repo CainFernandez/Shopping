4f42987b57a402078fbf1a348b15118e669037c5
CainPerez <cainasbel34@gmail.com>
2024-03-07 21:28:51 -0500

    Update CityViewModel.cs

--- Shooping/Shooping/Models/CityViewModel.cs
+++ Shooping/Shooping/Models/CityViewModel.cs
@@ -5,7 +5,7 @@ namespace Shooping.Models
     public class CityViewModel
     {
         public int Id { get; set; }
-        [Display(Name = "Departamento/Estado")]
+        [Display(Name = "Ciudad")]
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
