5df0ba89f786cbd6584923263a7287366d42f0ad
AbelPerezF <148727897+AbelPerezF@users.noreply.github.com>
2024-03-18 21:26:15 -0500

    Creaciòn controlador registro de usuario.

--- Shooping/Shooping/Controllers/AccountController.cs
+++ Shooping/Shooping/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shooping.Data;
+using Shooping.Data.Entities;
+using Shooping.Enums;
 using Shooping.Helpers;
 using Shooping.Models;
 
@@ -7,10 +10,16 @@ namespace Shooping.Controllers
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
+        private readonly DataContext _context;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public AccountController(IUserHelper userHelper) 
+        public AccountController(IUserHelper userHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper) 
         {
             _userHelper = userHelper;
+            _context = context;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
         }
         public IActionResult Login()
         {
@@ -44,6 +53,61 @@ namespace Shooping.Controllers
             await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        //================= REGISTRO DE USUARIO ===================//
+        public async Task<IActionResult> Register()
+        {
+            AddUserViewModel model = new()
+            {
+                Id = Guid.Empty.ToString(),
+                Countries = await _combosHelper.GetComboCountriesAsync(),
+                States = await _combosHelper.GetComboStatesAsync(0),
+                Cities = await _combosHelper.GetComboCitiesAsync(0),
+                UserType = UserType.User,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                model.ImageId = imageId;
+                User user = await _userHelper.AddUserAsync(model);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    return View(model);
+                }
+
+                LoginViewModel loginViewModel = new()
+                {
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
+
+                var result2 = await _userHelper.LoginAsync(loginViewModel);
+
+                if (result2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(model);
+        }
+
 
         //============== PÀGINA NO AUTORIZADA ======================//
         public IActionResult NotAuthorized()
